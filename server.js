725dab2a4e81057474600e49a615fc9b7a5f5155
Cristiano <cristianosantosreal@gmail.com>
2026-09-22 23:37:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,34 @@ app.post('/sales', async (request, response) => {
     try {
         const newMonthlySale = await MonthlySales.create(request.body)
         response.json(newMonthlySale)
+        
+    } catch (error) {
+        response.json({error: error})
+    }
+})
 
+app.get('/sales', async (request, response) => {
+    try {
+        const getMonthlySales = await MonthlySales.find()
+        response.json(getMonthlySales)
+    } catch (error) {
+        response.json({error: error})
+    }
+})
+
+app.put('/sales/:id', async (request, response) => {
+    try {
+        const newMonthlySales = await MonthlySales.findByIdAndUpdate(request.params.id, request.body, {new: true})
+        response.json(newMonthlySales)
+    } catch (error) {
+        response.json({error: error})
+    }
+})
+
+app.delete('/sales/:id', async (request, response) => {
+    try {
+        const deleteMonthlySales = await MonthlySales.findByIdAndDelete(request.params.id, request.body, {new: true})
+        response.json(deleteMonthlySales)
     } catch (error) {
         response.json({error: error})
     }
